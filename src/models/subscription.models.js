c9d610b43743b64d11f9976ccbab212995589f78
Nitinsaini1515 <gamern662@gmail.com>
2025-07-23 23:43:50 +0530

    refactor subscription model for improved code readability and formatting

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -2,16 +2,16 @@ import mongoose, { mongo, Schema } from "mongoose";
 
 const SubsShema = new mongoose.Schema(
   {
-    subscriber:{
-      type:Schema.Types.ObjectId,
-      ref:"User"
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    channel:{
-type:Schema.Types.ObjectId,
-ref:"User"
-    }
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-
-},{timestamps:true})
-
-export const Subscription = mongoose.model("Subscription",SubsShema)
+export const Subscription = mongoose.model("Subscription", SubsShema);
