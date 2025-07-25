f8c3f184dda91b021b7131c700892cebceb47bfe
Nitinsaini1515 <gamern662@gmail.com>
2025-07-25 22:39:22 +0530

    refactor user route for improved organization and readability

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,24 +1,45 @@
 import { Router } from "express";
-import {LoginUser, LogoutUser, RefreshAccessToken, RegisterUser} from "../controller/user.controller.js";
-import {upload} from '../middleware/multer.middlieware.js'
+import {
+  AvatarChange,
+  GetCurrentUser,
+  getUserChannelProfile,
+  LoginUser,
+  LogoutUser,
+  PasswordChange,
+  RefreshAccessToken,
+  RegisterUser,
+  UpdateCoverimage,
+  UsernameChange,
+  WatchHistory,
+} from "../controller/user.controller.js";
+import { upload } from "../middleware/multer.middlieware.js";
 import verifyJwt from "../middleware/auth.middleware.js";
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
   upload.fields([
-{
-name :"avatar",
-maxCount:1
-},
-{
-name:"coverimage",
-maxCount:1
-}
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverimage",
+      maxCount: 1,
+    },
   ]),
   RegisterUser
-
-)
-router.route("/login").post(LoginUser)
-router.route("/logout").post(verifyJwt,LogoutUser)
-router.route("/refreshtoken").post(RefreshAccessToken)
-export default router
+);
+router.route("/login").post(LoginUser);
+router.route("/logout").post(verifyJwt, LogoutUser);
+router.route("/refreshtoken").post(RefreshAccessToken);
+router.route("change-password").post(verifyJwt, PasswordChange);
+router.route("/current-user").get(verifyJwt, GetCurrentUser);
+router.route("avatar").patch(verifyJwt, upload.single("avatar"), AvatarChange);
+router
+  .route("cover-image")
+  .patch(verifyJwt, upload.single("coverimage"), UpdateCoverimage);
+router.route("/name-change").post(verifyJwt, UsernameChange);
+// router.route("/change-profile").patch(verifyJwt,upload.single())
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("history").get(verifyJwt, WatchHistory);
+export default router;
